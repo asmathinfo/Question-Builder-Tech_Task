541feef417b989c70f2e6753dadf0055de78b16f
Buhary Asmath <asmath.info@gmail.com>
2023-05-15 23:50:44 +0530

    overall Ui works done P1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-
+// this is a function for RichText EDitor TineyMce i am Using Trough API 
+//  this is a api i am using rg55f6v2tlz4q7j0j56daevysb599kjpvqfbtzsfgrd5465j
   tinymce.init({
     selector: 'textarea',
     plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
@@ -15,18 +16,14 @@
   });
 
 
-
-
+// this is a function for upload media 
   function uploadMedia() {
     var fileInput = document.getElementById('mediaUpload');
     var file = fileInput.files[0];
-    
-    // Perform necessary actions with the uploaded file
-    // For example, you can upload it to a server or process it locally
-    // Here, we'll simply display the file name
     alert('Selected file: ' + file.name);
   }
   
+  // this is a Save button function which we're using to save the questions
   var saveButton = document.getElementById('saveButton');
     saveButton.addEventListener('click', function() {
       var content = tinymce.get('myEditor').getContent();
@@ -34,9 +31,11 @@
       alert('Content saved!');
     });
 
+    // this where we are dynamically creating the textboxes when user add number of the options
     function addTextboxes() {
         var numOptions = parseInt(document.getElementById("numOptions").value);
         var answerBox = document.querySelector(".answerBox");
+        
         
         // Clear existing content in answerBox
         answerBox.innerHTML = "";
@@ -46,5 +45,14 @@
           textbox.type = "text";
           textbox.placeholder = "Type Option name " + (i + 1);
           answerBox.appendChild(textbox);
-        }
+
+          var removeButton = document.createElement("button");
+      removeButton.textContent = "-";
+      removeButton.className = "removeOptionBtn";
+      removeButton.onclick = function() {
+        answerBox.removeChild(textbox);
+        answerBox.removeChild(removeButton);
+        };
+        answerBox.appendChild(removeButton);
       }
+    }
