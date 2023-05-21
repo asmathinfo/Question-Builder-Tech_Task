26c8737c659a842b03979228547269f38483ee1b
Buhary Asmath <asmath.info@gmail.com>
2023-05-20 23:07:42 -0700

    overall works completed

--- script.js
+++ script.js
@@ -27,32 +27,91 @@
   var saveButton = document.getElementById('saveButton');
     saveButton.addEventListener('click', function() {
       var content = tinymce.get('myEditor').getContent();
-      // Implement your save functionality here
       alert('Content saved!');
     });
 
     // this where we are dynamically creating the textboxes when user add number of the options
-    function addTextboxes() {
-        var numOptions = parseInt(document.getElementById("numOptions").value);
-        var answerBox = document.querySelector(".answerBox");
+    // function addTextboxes() {
+    //     var numOptions = parseInt(document.getElementById("numOptions").value);
+    //     var answerBox = document.querySelector(".answerBox");
         
         
-        // Clear existing content in answerBox
-        answerBox.innerHTML = "";
+    //     answerBox.innerHTML = "";
         
-        for (var i = 0; i < numOptions; i++) {
-          var textbox = document.createElement("input");
-          textbox.type = "text";
-          textbox.placeholder = "Type Option name " + (i + 1);
-          answerBox.appendChild(textbox);
+    //     for (var i = 0; i < numOptions; i++) {
+    //       var textbox = document.createElement("input");
+    //       textbox.type = "text";
+    //       textbox.placeholder = "Type Option name " + (i + 1);
+    //       answerBox.appendChild(textbox);
 
-          var removeButton = document.createElement("button");
-      removeButton.textContent = "-";
-      removeButton.className = "removeOptionBtn";
-      removeButton.onclick = function() {
-        answerBox.removeChild(textbox);
-        answerBox.removeChild(removeButton);
-        };
-        answerBox.appendChild(removeButton);
-      }
+    //       var removeButton = document.createElement("button");
+    //   removeButton.textContent = "-";
+    //   removeButton.className = "removeOptionBtn";
+    //   removeButton.onclick = function() {
+    //     answerBox.removeChild(textbox);
+    //     answerBox.removeChild(removeButton);
+    //     };
+    //     answerBox.appendChild(removeButton);
+    //   }
+    // }
+ 
+// script.js
+
+// script.js
+
+function addItem() {
+  var userInput = document.getElementById('inputText').value;
+  var selectedDate = document.getElementById('inputDate').value;
+
+  var newItem = document.createElement('p');
+  newItem.textContent = userInput;
+  newItem.classList.add('resultTexts');
+
+  var newDate = document.createElement('p');
+  newDate.textContent = selectedDate;
+  newDate.classList.add('resultTexts');
+  var currentTime = new Date().toLocaleTimeString();
+  var timeSpan = document.createElement('span');
+timeSpan.textContent = ' ' + currentTime;
+
+
+  var resultsDiv = document.getElementById('resultText');
+  var dateDiv = document.getElementById('resultdatepicker');
+  var results = document.getElementById('results');
+
+
+  resultsDiv.appendChild(newItem);
+  dateDiv.appendChild(newDate);
+  newDate.appendChild(timeSpan);
+
+  results.style.display = 'flex';
+
+}
+
+
+
+function addTextboxes() {
+  var numOptions = parseInt(document.getElementById("numOptions").value);
+
+  // Show or hide the sectionheadBox div based on the user input
+  var sectionheadBox = document.getElementById("sectionheadBox");
+  if (numOptions > 0) {
+    sectionheadBox.classList.remove("hidden");
+
+    // Remove any existing cloned sectionheadBox divs
+    var clonedSectionheadBoxes = document.getElementsByClassName("clonedSectionheadBox");
+    while (clonedSectionheadBoxes.length > 0) {
+      clonedSectionheadBoxes[0].parentNode.removeChild(clonedSectionheadBoxes[0]);
     }
+
+    // Clone and append the sectionheadBox div the specified number of times
+    var section2 = document.getElementById("section2");
+    for (var i = 0; i < numOptions - 1; i++) {
+      var clonedSectionheadBox = sectionheadBox.cloneNode(true);
+      clonedSectionheadBox.classList.add("clonedSectionheadBox");
+      section2.appendChild(clonedSectionheadBox);
+    }
+  } else {
+    sectionheadBox.classList.add("hidden");
+  }
+}
